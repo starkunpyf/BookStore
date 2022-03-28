771f889de9bd0d339e78195f361504ac206fab38
starkunpyf <2560331078@qq.com>
2022-03-28 16:17:11 +0800

    test

---
+++ src/main/java/com/chongya/bookstoretest/BookstoreTestApplication.java
@@ -0,0 +1,13 @@
+package com.chongya.bookstoretest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chongya/bookstoretest/BookstoreTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chongya.bookstoretest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
